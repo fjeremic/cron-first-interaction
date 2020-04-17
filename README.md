6af67be87de109d1b9406c05abfb1c2ef9170ca6
Filip Jeremic <fjeremic@ca.ibm.com>
2020-04-17 17:59:19 -0400

    Implement cron based first-interaction
    Use actions/stale as an example of how to implement pagination and
    rate limiting support and adapt it to the actions/first-interaction
    repository.
    
    The cron based first-interaction sorts the pull request by date
    updated in descending order and processes them subject to the rate
    limit. The idea is that if the cron job runs often enough it will
    process all the pull requests since the most recently updated ones
    are processed first.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# cron-first-interaction
-An action for automatically commenting on pull requests and issues from first-time contributors.
+# Cron First Interaction
+
+Cron first interaction is an extension of [actions/first-interaction](https://github.com/actions/first-interaction) which automatically comments on pull requests from first-time contributors using a rate limit aware cron job.
+
+The [actions/first-interaction](https://github.com/actions/first-interaction) GitHub Action runs into the following issue (further described in [actions/first-interaction#10](https://github.com/actions/first-interaction/issues/10)) when the check runs on a pull request originating from a _forked_ repository:
+
+```
+##[error] HttpError: Resource not accessible by integration
+##[error] Resource not accessible by integration
+##[error] Node run failed with exit code 1
+```
+
+This is a fairly restrictive limitation in the GitHub Pull Request Workflow which many open source projects follow.
+
+This project circumvents this limitation by running the GitHub Action as a cron job on the target repository. The cron job continuously monitors the pull requests of the target repository and posts comments on pull requests from first time contributors in a rate limiting aware manner with pagination support. The idea is that if this action is run often enough it will keep monitoring the most recently updated pull requests, and eventually all pull requests will have been processed.
+
+To prevent a duplicate comment being posted on subsequent scheduled jobs, this action adds a label once the comment is posted which will be checked for on subsequent runs. It is up to the repository owner to ensure the label exists.
+
+## Usage
+
+### Create Workflow
+
+Create a workflow (eg: `.github/workflows/first-interaction.yml` see [Creating a Workflow file](https://help.github.com/en/articles/configuring-a-workflow#creating-a-workflow-file)) to utilize the first-interaction action running every 10 minutes:
+
+```
+name: "First Time Contributor"
+on:
+  schedule:
+  - cron: "*/10 * * * *"
+
+jobs:
+  triage:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: fjeremic/cron-first-interaction@0.1.0
+      with:
+        repo-token: ${{ secrets.GITHUB_TOKEN }}
+        pr-message: |-
+          Thank you for supporting the project $GITHUB_ACTOR, and congratulations on your
+          first contribution! A project committer will shortly review your contribution. 
+          In the mean time, if you haven't had a chance please skim over the 
+          [contribution guidelines](https://example.com/) which all pull requests must adhere to.
+
+          We hope to see you around!
+        pr-label:
+          - first contribution
+```
+
+_Note: This grants access to the `GITHUB_TOKEN` so the action can make calls to GitHub's rest API._
---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { Octokit } from "@octokit/rest";
+
+type Pull = Octokit.PullsListResponseItem;
+type PullLabel = Octokit.PullsListResponseItemLabelsItem;
+
+interface Args {
+  repoToken: string;
+  prMessage: string;
+  prLabel: string;
+  operationsPerRun: number;
+}
+
+async function run(): Promise<void> {
+  try {
+    const args = getAndValidateArgs();
+
+    const client = new github.GitHub(args.repoToken);
+
+    await processPrs(client, args, args.operationsPerRun);
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.stack);
+
+    core.setFailed(error.message);
+  }
+}
+
+async function processPrs(
+  client: github.GitHub,
+  args: Args,
+  operationsLeft: number,
+  page: number = 1
+): Promise<number> {
+  const prs = await client.pulls.list({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    state: "open",
+    sort: "updated",
+    direction: "desc",
+    per_page: 100,
+    page
+  });
+
+  operationsLeft -= 1;
+
+  if (prs.data.length === 0 || operationsLeft === 0) {
+    return operationsLeft;
+  }
+
+  for (const pr of prs.data.values()) {
+    console.log(`found pr #${pr.number}: ${pr.title}`);
+
+    if (isLabeled(pr, args.prLabel)) {
+      console.log(`skipping pr #${pr.number} due to label ${args.prLabel}`);
+      continue;
+    }
+
+    if (await isFirstPull(client, pr.user.login, pr.number)) {
+      console.log(`adding comment to pr #${pr.number}`);
+
+      await client.pulls.createReview({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number,
+        body: args.prMessage,
+        event: "COMMENT"
+      });
+
+      console.log(`marking pr #${pr.number} with ${args.prLabel} label`);
+
+      await client.issues.addLabels({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        issue_number: pr.number,
+        labels: [args.prLabel]
+      });
+
+      operationsLeft -= 2;
+    }
+
+    // For the isFirstPull call
+    operationsLeft -= 1;
+
+    if (operationsLeft < 3) {
+      core.warning(
+        `performed ${args.operationsPerRun} operations, exiting to avoid rate limit`
+      );
+      return 0;
+    }
+  }
+
+  return await processPrs(client, args, operationsLeft, page + 1);
+}
+
+function isLabeled(pr: Pull, label: string): boolean {
+  const labelComparer: (l: PullLabel) => boolean = l =>
+    label.localeCompare(l.name, undefined, { sensitivity: "accent" }) === 0;
+  return pr.labels.filter(labelComparer).length > 0;
+}
+
+async function isFirstPull(
+  client: github.GitHub,
+  sender: string,
+  prlNumber: number
+): Promise<boolean> {
+  const { status, data: issues } = await client.issues.listForRepo({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    creator: sender,
+    state: "all"
+  });
+
+  if (status !== 200) {
+    throw new Error(`Received unexpected API status code ${status}`);
+  }
+
+  for (const issue of issues) {
+    if (issue.number < prlNumber && issue.pull_request) {
+      console.log(
+        `skipping pr #${prlNumber} because author has created ${issues.length} prs in the past`
+      );
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function getAndValidateArgs(): Args {
+  const args = {
+    repoToken: core.getInput("repo-token", { required: true }),
+    prMessage: core.getInput("pr-message", { required: true }),
+    prLabel: core.getInput("pr-label", { required: true }),
+    operationsPerRun: parseInt(
+      core.getInput("operations-per-run", { required: true })
+    )
+  };
+
+  for (const numberInput of ["operations-per-run"]) {
+    if (isNaN(parseInt(core.getInput(numberInput)))) {
+      throw Error(`input ${numberInput} did not parse to a valid integer`);
+    }
+  }
+
+  return args;
+}
+
+run();
